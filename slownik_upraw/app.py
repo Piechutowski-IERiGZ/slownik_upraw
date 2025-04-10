e5eea685a44e83133cd38dbb67014c2db401a415
Simon <szymon.piechutowski@ierigz.waw.pl>
2025-04-10 13:39:14 +0200

    .

--- slownik_upraw/app.py
+++ slownik_upraw/app.py
@@ -2,15 +2,20 @@ import itertools as it
 import sqlite3 as s3
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Sequence
 
 
-from litestar import Litestar, get
+from litestar import Litestar, MediaType, get
+from litestar.types import ControllerRouterHandler
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.static_files import create_static_files_router
 from litestar.template import TemplateConfig
 from litestar.response import Template
 
 from slownik_upraw.db import run_fixture, connection, close_connection
+
+
+HTML_MEDIA: str = MediaType.HTML
 
 
 @dataclass
@@ -59,21 +64,17 @@ for x in range(1, 4):
 
 @get("/")
 async def index() -> Template:
-    return Template(template_name="base.jinja")
-
-@get("/crops")
-async def crops() -> Template:
-    return Template(template_name="base.jinja", context={"kategorie": lista_kategorii})
+    return Template(template_name="slownik.jinja", context={"kategorie": lista_kategorii}, media_type=HTML_MEDIA)
 
 
 @get("/jak-to-dziala")
 async def jak_to_dziala() -> Template:
-    return Template(template_name="jak_to_dziala.jinja")
+    return Template(template_name="jak_to_dziala.jinja", media_type=HTML_MEDIA)
 
 
-@get("/slownik")
+@get("/api")
 async def slownik() -> Template:
-    return Template(template_name="slownik.jinja")
+    return Template(template_name="api.jinja", media_type=HTML_MEDIA)
 
 
 
@@ -82,12 +83,13 @@ template_config = TemplateConfig(
     engine=JinjaTemplateEngine,
 )
 
-
+route_handlers: Sequence[ControllerRouterHandler] = [
+    index, jak_to_dziala, slownik,
+    create_static_files_router(directories=[Path(__file__).resolve().parent.parent / "static"], path="/static")
+]
 
 app = Litestar(
-    route_handlers=[
-        index, jak_to_dziala, slownik, crops,
-        create_static_files_router(directories=[Path(__file__).resolve().parent.parent / "static"], path="/static")],
+    route_handlers=route_handlers,
     template_config=template_config,
     debug=True,
     plugins=[],

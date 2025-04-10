e2e553d23cbdf3512e170343a9ff59e70a737cb4
Simon <szymon.piechutowski@ierigz.waw.pl>
2025-04-10 15:10:11 +0200

    .

--- static/collapsible.js
+++ static/collapsible.js
@@ -1,15 +1,15 @@
 "use strict";
-function toggle_collapse(event) {
+function toggleCollapse(event) {
     const container = event.parentElement.children[1];
     if (container.classList.contains("hidden")) {
         // Open
         container.classList.remove("hidden", "max-h-0");
-        container.classList.add("max-h-fit");
+        container.classList.add("flex", "max-h-fit");
         event.textContent = "📂" + event.textContent.slice(2);
     }
     else {
         // Close
-        container.classList.remove("max-h-fit");
+        container.classList.remove("flex", "max-h-fit");
         container.classList.add("hidden", "max-h-0");
         event.textContent = "📁" + event.textContent.slice(2);
     }
